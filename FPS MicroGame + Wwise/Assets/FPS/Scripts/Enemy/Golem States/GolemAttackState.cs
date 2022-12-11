ee542223f7c2f02e52ee510cf705674d4c4cd2f8
PetersonNormil <petersonnormil@gmail.com>
2022-12-10 22:28:59 -0500

    FINETUNING GOLEM STATS

--- FPS MicroGame + Wwise/Assets/FPS/Scripts/Enemy/Golem States/GolemAttackState.cs
+++ FPS MicroGame + Wwise/Assets/FPS/Scripts/Enemy/Golem States/GolemAttackState.cs
@@ -9,38 +9,45 @@ public class GolemAttackState : MonoBehaviour, IGolemBaseState
     [SerializeField] private GolemController gc; //potential problem, what if there is multiple bosses?
     [SerializeField] private GolemIdleState idleState;
 
-    [SerializeField] private bool hasGolemAttacked = false;
     [SerializeField] private float golemAttackDamage = 10f;
+    [SerializeField] private float attackPace = 2f; 
     [Header("Attack box area for golem smash attack")] 
     [SerializeField] private Transform smashAttackCenter;
 
+    [SerializeField] private float attackRadiusSize = 5f;
     [SerializeField] private LayerMask playerLayer;//assumption for now, is that golem can only attack player, will probs be refactored to be able to attack ally robots as well
+    private float timer = 0.0f;
     private void Start()
     {
+        attackPace = 1 / attackPace; //inverting, so its easier to understand for game design
         gc = FindObjectOfType<GolemController>();
     }
 
     public void Enter()
     {
-        gc.GolemAnimation.OnAttack();
-        hasGolemAttacked = false;
+        timer = 0; 
+        gc.GolemAnimation.OnAttack(); //this starts the animatino that plays with an Event  'GolemAttackLogic' 
     }
 
     public void Exit()
     {
-        hasGolemAttacked = false;
     }
 
      void IGolemBaseState.DoState()
-    {
-        
+     {
+         timer += Time.deltaTime; 
        
         //go right back to idle state
         if (gc.PlayerDetect.IsPlayerInCollider() == true)
         {
             Debug.Log("stayin in attack");
-            //continue
+            
             //some sort of timer system, where you attack every x amount of sec
+            if (timer > attackPace)
+            {
+                gc.GolemAnimation.OnAttack();
+                timer = 0;
+            }
         }
         else
         {
@@ -53,11 +60,10 @@ public class GolemAttackState : MonoBehaviour, IGolemBaseState
 
      public void GolemAttackLogic()
      {
-         if (hasGolemAttacked) return;
          if ((GolemAttackState)gc.currentState != this) return;
          
          //transform.localScale / 2 is half the size of the scale of the gameobject 
-         Collider[] hitColliders = Physics.OverlapBox(smashAttackCenter.position, transform.localScale * 4, Quaternion.identity, playerLayer);
+         Collider[] hitColliders = Physics.OverlapBox(smashAttackCenter.position, transform.localScale * attackRadiusSize, Quaternion.identity, playerLayer);
          int i = 0;
          //Check when there is a new collider coming into contact with the box
          while (i < hitColliders.Length)
@@ -83,6 +89,6 @@ public class GolemAttackState : MonoBehaviour, IGolemBaseState
          //Check that it is being run in Play Mode, so it doesn't try to draw this in Editor mode
         
              //Draw a cube where the OverlapBox is (positioned where your GameObject is as well as a size)
-             Gizmos.DrawWireCube(smashAttackCenter.position, transform.localScale * 3);
+             Gizmos.DrawWireCube(smashAttackCenter.position, transform.localScale * attackRadiusSize);
      }
 }
--- FPS MicroGame + Wwise/Assets/FPS/Scripts/Enemy/Golem States/GolemChaseState.cs
+++ FPS MicroGame + Wwise/Assets/FPS/Scripts/Enemy/Golem States/GolemChaseState.cs
@@ -8,6 +8,11 @@ public class GolemChaseState : MonoBehaviour, IGolemBaseState
     [SerializeField] private GolemController gc;
     [SerializeField] private Transform playerLocation;
     [SerializeField] private GolemAttackState attackState;
+
+    [Header("NavMesh Init stats")] 
+    [SerializeField] private float speed = 10.0f;
+
+    [SerializeField] private float acceleration = 10.0f;
     public NavMeshAgent NavMeshAgent { get; private set; }
 
     // Start is called before the first frame update
@@ -15,6 +20,8 @@ public class GolemChaseState : MonoBehaviour, IGolemBaseState
     {
         gc = FindObjectOfType<GolemController>();
         NavMeshAgent = GetComponentInParent<NavMeshAgent>();
+        NavMeshAgent.speed = speed;
+        NavMeshAgent.acceleration = acceleration;
     }
 
     // Update is called once per frame
